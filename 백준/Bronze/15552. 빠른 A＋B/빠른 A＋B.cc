7a3d0d38ee9b2b7444e176ae25cafe80698ab635
woodool <woojin317@gmail.com>
2025-03-22 23:11:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int c;
+	cin >> c;
+	int a, b;
+	for (int i = 0; i < c; i++) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
