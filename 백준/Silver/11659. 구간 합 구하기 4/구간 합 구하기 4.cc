d223df01be3d03125c4713e9c86f388b815f444d
woodool <woojin317@gmail.com>
2025-03-30 01:43:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+	vector<int> Vec(N + 1, 0);
+
+	for (int i = 1; i <= N; i++) {
+		int a;
+		cin >> a;
+		Vec[i] = Vec[i - 1] + a;
+	}
+
+	while (M--) {
+		int b, c;
+		cin >> b >> c;
+		cout << Vec[c] - Vec[b - 1] << '\n';
+	}
+}
