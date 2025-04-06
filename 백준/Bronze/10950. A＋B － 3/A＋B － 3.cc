feb5f0bdee0ae1a91eb1ed9ab803e4a39cf6cbd2
woodool <woojin317@gmail.com>
2025-04-06 23:51:43 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, a, b;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
